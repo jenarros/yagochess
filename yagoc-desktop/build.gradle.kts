70602547c50b331dc52f7d89dcbdc487a447b867
Eduardo Narros <jenarros@gmail.com>
2021-02-19 13:05:25 +0000

    Only package shadow jar inside mac image.

--- yagoc-desktop/build.gradle.kts
+++ yagoc-desktop/build.gradle.kts
@@ -44,27 +44,42 @@ tasks.register("runApp", JavaExec::class) {
     main = yagocClass
 }
 
-tasks.register("createOSXImage", Exec::class) {
+val jpackageDefaults = mutableListOf(
+    System.getenv("JAVA_HOME") + "/bin/jpackage",
+    "--name", "yagochess",
+    "--input", project.buildDir.absolutePath + "/libs",
+    "--main-jar", "yagoc-desktop-" + project.version + "-all.jar",
+    "--main-class", yagocClass,
+    "--add-modules", "java.desktop",
+    "--java-options", "--enable-preview",
+    "--dest", project.buildDir.absolutePath + "/distributions",
+    "--description", "Yagochess is a game of chess focused on simplicity of both design and implementation.",
+    "--vendor", "José Eduardo Narros Martínez",
+    "--copyright", "Copyright 2021 José Eduardo Narros Martínez. All rights reserved.",
+    "--app-version", project.version.toString(),
+    "--resource-dir", projectDir.absolutePath + "/src/main/resources",
+    "--verbose"
+)
+
+tasks.register("createMacImage", Exec::class) {
     dependsOn(tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>())
     description = "Build the OSX DMG package."
 
     commandLine(
-        listOf(
-            System.getenv("JAVA_HOME") + "/bin/jpackage",
-            "--name", "yagochess",
-            "--input", project.buildDir.absolutePath + "/libs",
-            "--main-jar", "yagoc-desktop-" + project.version + "-all.jar",
-            "--main-class", yagocClass,
-            "--add-modules", "java.desktop",
-            "--type", "dmg",
-            "--java-options", "--enable-preview",
-            "--dest", project.buildDir.absolutePath + "/distributions",
-            "--license-file", "../LICENSE",
-            "--description", "Yagochess is a game of chess focused on simplicity of both design and implementation.",
-            "--vendor", "José Eduardo Narros Martínez",
-            "--app-version", project.version,
-            "--resource-dir", projectDir.absolutePath + "/src/main/resources",
-            "--verbose"
+        jpackageDefaults.plus(listOf("--type", "app-image"))
+    )
+}
+
+tasks.register("createMacDmg", Exec::class) {
+    dependsOn(tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>())
+    description = "Build the OSX DMG package."
+
+    commandLine(
+        jpackageDefaults.plus(
+            listOf(
+                "--type", "dmg",
+                "--license-file", "../LICENSE"
+            )
         )
     )
 }
