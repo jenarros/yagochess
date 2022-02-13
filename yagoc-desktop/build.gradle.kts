a92e156bee7ca75f483bc9d9dc37af78a97650bc
Eduardo Narros <jenarros@gmail.com>
2022-02-13 18:26:17 +0000

    Removed jcenter repository.

--- yagoc-desktop/build.gradle.kts
+++ yagoc-desktop/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.30"
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     java
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
